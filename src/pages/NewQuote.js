40583b61fd26d16468d5524843ced510517b3f47
prashanthdsouza <79136584+prashanthdsouza@users.noreply.github.com>
2022-09-21 18:27:40 +0530

    281

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,3 +1,11 @@
-const NewQuote = () => {};
+import QuoteForm from '../components/quotes/QuoteForm';
+
+const NewQuote = () => {
+    const onAddQuoteHandler = quoteData => {
+
+    };
+
+    return <QuoteForm onAddQuote= {onAddQuoteHandler} />
+};
 
 export default NewQuote;
